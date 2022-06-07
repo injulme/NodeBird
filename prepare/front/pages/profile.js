3ab4bbbbd28311b58a654c246d278b3f46396281
hyojin <injulme@naver.com>
2022-06-07 23:13:27 +0900

    styled-components 사용, antd 사용, 로그인/사용자 폼 컴포넌트 구현

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,10 +1,14 @@
+import Head from "next/head";
 import React from "react";
 import AppLayout from "../components/AppLayout";
 const Profile = () => {
   return (
-    <AppLayout>
-      <div>내 프로필</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>내 프로필</AppLayout>
+    </>
   );
 };
 
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,10 +1,14 @@
+import Head from "next/head";
 import React from "react";
 import AppLayout from "../components/AppLayout";
 const Signup = () => {
   return (
-    <AppLayout>
-      <div>회원가입 페이지</div>
-    </AppLayout>
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>회원가입 페이지</AppLayout>
+    </>
   );
 };
 
